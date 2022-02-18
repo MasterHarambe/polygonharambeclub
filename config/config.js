ef2af1dead4dce2707ba850b16f199691aeb99c5
MasterHarambe <99116899+MasterHarambe@users.noreply.github.com>
2022-02-18 19:36:23 +0630

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const config = {
+    app_name: 'Harambe Club NFT',
+    app_description: 'Enjoy staking, breeding, P2E trading card game, tokenomics, and we plan to take over The Sandbox metaverse. 10,000 original Harambe Club NFTs are stored as ERC-721 tokens. 5,000 of them are on the Polygon Network and the other 5,000 are on the Ethereum Network. All Harambes are hosted on IPFS.',
+    collection_file_name: 'harambeclub.json',
+    collection_contract_address: '0xeCbd4de8c49c6caA2bcfbc8F8B18Ecbc24F8DADe',
+    collection_name: 'Harambe Club NFT',
+    collection_description: 'Enjoy staking, breeding, P2E trading card game, tokenomics, and we plan to take over The Sandbox metaverse. 10,000 original Harambe Club NFTs are stored as ERC-721 tokens. 5,000 of them are on the Polygon Network and the other 5,000 are on the Ethereum Network. All Harambes are hosted on IPFS.',
+    collection_id_from: 5001,
+    ignore_traits: ['date'], 
+    sqlite_file_name: 'database.sqlite',
+    ga: 'G-BW69Z04YTP',
+    main_og_image: 'https://ibb.co/pRt3065',
+    item_path_name: 'HC',
+    page_item_num: 60,
+    content_image_is_video: false,
+    content_image_frame: 'circle', // circle, rectangle
+    use_wallet: false
+};
+
+module.exports = config;
